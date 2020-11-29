18a9ac0ba557899e0bef2a6524cc06855e08c4c6
Fedor <f.alexeeff2010@yandex.ru>
2020-11-29 21:46:57 +0300

    нахождение суммы ряда

--- lab_5/main.c
+++ lab_5/main.c
@@ -1,16 +1,24 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <math.h>
 
 int main()
 {
-    short int x;
-    float e;
+    short int n=1;
+    float e, x, a=1, sum=a;
 
     printf("Введите x и точность: ");
-    while (scanf("%hd %e", &x, &e)!=2) {
+    while (scanf("%f %e", &x, &e)!=2) {
         while (getchar()!='\n');
         printf("Введены некорректные данные!\nВведите x и точность: ");
     }
 
+    while (fabs(a)>e) {
+        printf ("n=%d\t a=%lf\t sum=%.5f\n",n,a,sum);
+        a*=(-(n+1)*x*x)/(2*n*n*(2*n+1));
+        sum+=a;
+        n++;
+    }
+    printf ("n=%d\t a=%lf\t sum=%.5f\n",n,a,sum);
     return 0;
 }
