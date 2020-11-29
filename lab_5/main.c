3de70866bce5cab6e7545e25ca261b674b61b6e2
Fedor <f.alexeeff2010@yandex.ru>
2020-11-29 21:01:38 +0300

    ввод точности и x

--- lab_5/main.c
+++ lab_5/main.c
@@ -3,6 +3,14 @@
 
 int main()
 {
-    printf("Hello world!\n");
+    short int x;
+    float e;
+
+    printf("Введите x и точность: ");
+    while (scanf("%hd %e", &x, &e)!=2) {
+        while (getchar()!='\n');
+        printf("Введены некорректные данные!\nВведите x и точность: ");
+    }
+
     return 0;
 }
