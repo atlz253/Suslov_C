69503466e3b2b3429fb7c8a40a6d0cce7173b3e3
Fedor <f.alexeeff2010@yandex.ru>
2020-11-30 21:13:12 +0300

    ввод кол-ва элементов массива

--- lab_6/main.c
+++ lab_6/main.c
@@ -1,8 +1,22 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#define PART 0
+
+#if PART == 0
+
 int main()
 {
-    printf("Hello world!\n");
-    return 0;
+    unsigned short int n;
+    printf("Введите кол-во элементов массива: ");
+    while (scanf("%hu", &n)!=1) {
+        while (getchar()!='\n');
+        printf("Неверные данные! Введите кол-во элементов массива: ");
+    }
 }
+
+#elif PART == 1
+
+#elif PART == 2
+
+#endif // PART
