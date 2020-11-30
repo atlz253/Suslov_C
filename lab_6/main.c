4377c7aea0e4b15c514fe7b2ee063175f2e9477d
Fedor <f.alexeeff2010@yandex.ru>
2020-11-30 22:18:53 +0300

    удаление макс отрицательного числа и вывод

--- lab_6/main.c
+++ lab_6/main.c
@@ -14,14 +14,34 @@ int main()
         printf("Неверные данные! Введите кол-во элементов массива: ");
     }
 
-    short int arr[n];
+    short int arr[n], min=0;
     for (i=0;i<n;i++) {
         printf("Введите значение %hd: ", i);
         while (scanf("%hd", &arr[i])!=1) {
             while (getchar()!='\n');
             printf("Неверные данные! Введите значение %hd: ", i);
         }
+
+        if (arr[min]<0&&arr[i]<0&&arr[i]>arr[min]||arr[min]>=0) {
+            min=i;
+        }
     }
+
+    if (arr[min]<0) {
+        printf("Исходный массив: ");
+        for (i=0;i<n;printf("%d ", arr[i++]));
+
+        printf("\nНовый массив: ");
+        for (i=0;i<n;i++) {
+            if (i==min||arr[min]==arr[i]) continue;
+            printf("%d ", arr[i]);
+        }
+    }
+    else {
+        printf("В массиве нет отрицательных чисел!\n");
+    }
+
+    return 0;
 }
 
 #elif PART == 1
