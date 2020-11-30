1423d3ecb5d2ccb6fbac918bd43e6fc9fa0ff3c6
Fedor <f.alexeeff2010@yandex.ru>
2020-11-30 19:15:31 +0300

    ряд начинается с 0

--- lab_5/main.c
+++ lab_5/main.c
@@ -4,20 +4,20 @@
 
 int main()
 {
-    short int n=1;
+    short int n=0;
     float e, x, a=1, sum=a;
 
     printf("Введите x и точность: ");
-    while (scanf("%f %e", &x, &e)!=2) { // 3 10e-5
+    while (scanf("%f %e", &x, &e)!=2) {
         while (getchar()!='\n');
         printf("Введены некорректные данные!\nВведите x и точность: ");
     }
 
     while (fabs(a)>e) {
         printf ("n=%d\t a=%lf\t sum=%.5f\n",n,a,sum);
+        n++;
         a*=(-(n+1)*x*x)/(2*n*n*(2*n+1));
         sum+=a;
-        n++;
     }
     printf ("n=%d\t a=%lf\t sum=%.5f\n",n,a,sum);
     return 0;
