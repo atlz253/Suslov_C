8a0a44aa2dd6db8b50cc98de73821bdd18c21b82
Fedor <f.alexeeff2010@yandex.ru>
2020-12-07 21:21:36 +0300

    удаление лишних пробелов из строки

--- lab_7/main.c
+++ lab_7/main.c
@@ -7,12 +7,27 @@
 
 int main()
 {
+    short int a, i;
     char str[81], result[81];
 
     printf("Введите строку (не более 80 символов): ");
     fgets(str, sizeof str, stdin);
 
+    for (i=0;i<81; i++) {
+        if (str[i]==' '&&str[i-1]==' ') {
+            a++;
+            continue;
+        }
+        else {
+            result[i-a] = str[i];
+        }
+    }
+
+    printf("Исходная строка: ");
     puts(str);
+
+    printf("Резултат: ");
+    puts(result);
 
     return 0;
 }
