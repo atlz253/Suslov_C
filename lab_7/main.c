4dec1cae61947ab72a0923ac2e8140beb1fed489
Fedor <f.alexeeff2010@yandex.ru>
2020-12-08 00:06:37 +0300

    ввод строк в программе 2

--- lab_7/main.c
+++ lab_7/main.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#define PART 0
+#define PART 1
 
 #if PART == 0
 
@@ -11,7 +11,7 @@ int main()
     char str[81], result[81];
 
     printf("Введите строку (не более 80 символов): ");
-    fgets(str, sizeof str, stdin);
+    fgets(str, 81, stdin);
 
     for (i=0;i<81; i++) {
         if (str[i]==' '&&str[i-1]==' ') {
@@ -33,5 +33,36 @@ int main()
 }
 
 #elif PART == 1
+
+int main()
+{
+    short int n, i;
+
+    do {
+        printf("Введите количество строк: ");
+        while (scanf("%hd", &n)!=1) {
+            while (getchar()!='\n');
+            printf("Введены неверные данные!\nВведите количество строк: ");
+        }
+        if (n>6) printf("Максимальное количество строк - 6.\nВведите количество строк: ");
+        if (n<1) printf("Минимальное количество строк - 1.\nВведите количество строк: ");
+    } while(n>6||n<1);
+    getchar();
+
+    char str[n][81], result[n][81];
+
+    for (i=0;i<n;i++) {
+        printf("Введите строку (не более 80 символов): ");
+        //scanf("%80s", str[i]);
+        fgets(str[i], 81, stdin);
+    }
+
+    for (i=0;i<n;i++) {
+        printf("Исходная строка: ");
+        puts(str[i]);
+    }
+
+    return 0;
+}
 
 #endif
