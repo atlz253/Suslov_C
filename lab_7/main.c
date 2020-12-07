1b2d453e036c76b2cf99c20c16f88f0394c3ad65
Fedor <f.alexeeff2010@yandex.ru>
2020-12-07 21:06:29 +0300

    ввод строки

--- lab_7/main.c
+++ lab_7/main.c
@@ -1,8 +1,22 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#define PART 0
+
+#if PART == 0
+
 int main()
 {
-    printf("Hello world!\n");
+    char str[81], result[81];
+
+    printf("Введите строку (не более 80 символов): ");
+    fgets(str, sizeof str, stdin);
+
+    puts(str);
+
     return 0;
 }
+
+#elif PART == 1
+
+#endif
