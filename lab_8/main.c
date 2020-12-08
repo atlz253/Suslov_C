ca1d04314584cad86fd4857f11436eaaf125acf6
Fedor <f.alexeeff2010@yandex.ru>
2020-12-08 13:11:23 +0300

    макрос для переключения между программами

--- lab_8/main.c
+++ lab_8/main.c
@@ -1,8 +1,22 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#define PART 0
+
+#if PART == 0
+
 int main()
 {
     printf("Hello world!\n");
     return 0;
 }
+
+#elif PART == 1
+
+int main()
+{
+    printf("Hello world!\n");
+    return 0;
+}
+
+#endif
