691963cc8765fb6cdd4e44aa0e9b3fd0ebedc6b1
Fedor <f.alexeeff2010@yandex.ru>
2020-12-08 16:12:09 +0300

    ввод строки

--- lab_8/main.c
+++ lab_8/main.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 #define PART 0
 
@@ -7,7 +8,18 @@
 
 int main()
 {
-    printf("Hello world!\n");
+    short int i;
+    char str[81], *p;
+
+    do {
+        for (p=str;p!=str+80;*p++='\0');
+        printf("Введите строку (не более 80 символов): ");
+        gets(str);
+        if (strlen(str)>=81&&str[80]!='\0') printf("Введено больше 80 символов!\n");
+    } while (strlen(str)>=81&&str[80]!='\0');
+    
+    
+
     return 0;
 }
 
