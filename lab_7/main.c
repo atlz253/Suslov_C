25aa573ceb4abcda4aa3fca560bc9b71b31b7dc7
Fedor <f.alexeeff2010@yandex.ru>
2020-12-08 16:08:19 +0300

    упростил обнуление строк

--- lab_7/main.c
+++ lab_7/main.c
@@ -9,7 +9,7 @@
 int main()
 {
     short int a, i;
-    char str[81]={'\0'}, result[81]={'\0'};
+    char str[81], result[81];
 
     do {
         for (i=0;i<81;str[i++]='\0');
@@ -57,8 +57,6 @@ int main()
     getchar();
 
     char str[n][81], result[n][81];
-    memset(str,'\0',sizeof(char)*n*81);
-    memset(result,'\0',sizeof(char)*n*81);
 
     for (i=0;i<n;i++) {
         #if STANDARTFUNC == 0
