9bd783bbeba7176a928725d7963b13eab4560d7f
Fedor <f.alexeeff2010@yandex.ru>
2020-12-11 00:31:52 +0300

    функция ввода

--- lab_10/main.c
+++ lab_10/main.c
@@ -1,8 +1,24 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+void input(float num, char str[]) {
+    printf("%s", str);
+    while (scanf("%f", &num)!=1) {
+        while(getchar()!='\n');
+        printf("Неверные данные!\n%s", str);
+    }
+}
+
 int main()
 {
-    printf("Hello world!\n");
+    float a, b, eps, i, y, x;
+    input(a, "Введите a: ");
+    input(b, "Введите b: ");
+    input(eps, "Введите точность: ");
+
+    for (i=0;i<3;i++) {
+
+    }
+
     return 0;
 }
