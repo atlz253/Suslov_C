100e965d87769c7deea9bb45ddd14aa007e19e3a
Fedor <f.alexeeff2010@yandex.ru>
2020-12-11 15:44:14 +0300

    открытие и закрытие файла

--- lab_11/main.c
+++ lab_11/main.c
@@ -2,9 +2,12 @@
 #include <stdlib.h>
 
 FILE *f;
+char buffer[100];
 
-int open(FILE *file, char path[]) {
-    if ((file = fopen(path, "r+"))==NULL) {
+int open(FILE **file, char path[]) {
+    if ((*file = fopen(path, "r+"))==NULL) {
+        printf("Не удалось открыть файл!\n");
+        getchar();
         return 0;
     }
     else {
@@ -13,11 +16,20 @@ int open(FILE *file, char path[]) {
 }
 
 
+void close() {
+    if (f != NULL) {
+        printf("Файл закрыт!\n");
+        fclose(f);
+    }
+}
+
 
 int main()
 {
-    if (open(f, "test.txt")) {
-
+    if (open(&f, "test.txt")) {
+        fgets(buffer, 127, f);
+        printf("%s", buffer);
+        close();
     }
     else {
         printf("Не удалось открыть файл!\n");
