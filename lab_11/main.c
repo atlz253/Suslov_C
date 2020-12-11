063f096412ce4e0f7916535a7ec8b8184388dd97
Fedor <f.alexeeff2010@yandex.ru>
2020-12-11 15:23:11 +0300

    функция открытия файла

--- lab_11/main.c
+++ lab_11/main.c
@@ -1,8 +1,27 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+int open(FILE *file, char path[]) {
+    if ((file = fopen(path, "r+"))==NULL) {
+        return 0;
+    }
+    else {
+        return 1;
+    }
+}
+
 int main()
 {
-    printf("Hello world!\n");
+    FILE *file;
+
+    if (open(file, "test.txt")) {
+
+    }
+    else {
+        printf("Не удалось открыть файл!\n");
+        getchar();
+        return 0;
+    }
+
     return 0;
 }
